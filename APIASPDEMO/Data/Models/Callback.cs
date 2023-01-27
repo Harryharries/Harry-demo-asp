1cb10a7dbe54fe76f289934e7ba386c7ac6fc0db
HarryGu <harry.guapi@gmail.com>
2023-01-26 20:57:53 -0600

    configure db connection

---
+++ APIASPDEMO/Data/Models/Callback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace _7XConnectApi.Models
+{
+    public class Event
+    {
+        public long Id { get; set; }
+        
+        [Required]
+        public string EventName { get; set; }
+        
+        [Required]
+        public DateTime EventTime { get; set; }
+
+        [Required]
+        public DateTime EventEndTime { get; set; }
+
+        [StringLength(1000)]
+        public string Memo { get; set; }
+
+    }
+}
